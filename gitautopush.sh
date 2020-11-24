c150d54164bbbd3528075d1e7a5380dd0b203f0a
chenchuan <1350581922@qq.com>
2020-11-24 23:56:49 +0800

    format

---
+++ gitautopush.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#author=chenchuan
+#mail=1350581922@qq.com
+##################################
+
+# 上面中的 #! 是一种约定标记, 它可以告诉系统这个脚本需要什么样的解释器来执行;
+
+echo "gitautopush start..."
+git add .
+git commit -m '更新'
+echo "git提交注释: 更新"
+git push origin master
+echo "gitautopush end..."
