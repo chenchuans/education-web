6c0cb89010f07c81f63e14941a411ba49846afd5
chenchuan <1350581922@qq.com>
2020-12-06 14:43:52 +0800

    按钮组件封装

---
+++ src/components/button/Index.vue
@@ -0,0 +1,53 @@
+<template>
+    <span :style="{width: `${width}px`, height: `${height}px`, lineHeight: `${height}px`}" :class="[type, 'button']" @click="handleClick"><slot/></span>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+    name: 'Button',
+    props: {
+        width: {
+            type: Number,
+            default: 90
+        },
+        height: {
+            type: Number,
+            default: 30
+        },
+        type: {
+            type: String,
+            default: 'light'
+        }, 
+        params: {
+            type: Object,
+            default: () => {}
+        }
+    },
+    methods: {
+        handleClick() {
+            this.$emit('click', this.params);
+        }
+    }
+})
+export default class Button extends Vue {}
+</script>
+<style lang="scss" scoped>
+$fontColor: #fa8919;
+.button {
+    font-size: 14px;
+    font-weight: 400;
+    display: inline-block;
+    text-align: center;
+    border-radius: 5px;
+}
+.light {
+    color: $fontColor;
+    border: 1px solid $fontColor;
+}
+.dark {
+    background-color: $fontColor;
+    color: #fff;
+}
+</style>
--- src/layout/HomeBar.vue
+++ src/layout/HomeBar.vue
@@ -4,16 +4,21 @@
             <ul class="home-bar-wrapper-menu">
                 <li :class="currentRoute === item.path ? 'select-item' : ''" v-for="(item, index) in routeList" :key="index" class="home-bar-wrapper-menu-item" @click="toRouteLink(item)">{{item.name}}</li>
             </ul>
+            <div class="login">
+                <span class="login-user">用户名: {{userInfo.userName}}</span>
+                <ed-button class="login-button">{{loginStatus}}</ed-button>
+                <!-- <span class="login-button">{{loginStatus}}</span> -->
+            </div>
         </header>
     </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
+import EdButton from '@/components/button/Index.vue';
 
 @Options({
-    components: {
-    },
+    components: { EdButton },
     data() {
         return {
             routeList: [
@@ -21,11 +26,12 @@ import { Options, Vue } from "vue-class-component";
                 {name: '我的课程', path: '/content-my'},
                 {name: '个人中心', path: '/user-center'},
             ],
-            currentRoute: this.$route.path || '/home'
+            currentRoute: this.$route.path || '/home',
+            loginStatus: '登录',
+            userInfo: {
+                userName: '沉船'
+            }
         }
-    },
-    created() {
-        console.log(111, this.$route.path);
     },
     methods: {
         toRouteLink(item) {
@@ -40,7 +46,6 @@ export default class HomeBar extends Vue {
 </script>
 <style lang="scss" scoped>
 // @import url('../styles/variables.scss');
-// $homeBarBorderColor
 $fontColor: #fa8919;
 $orangeBackground: #fbf5ee;
 $homeBarBorderColor: #eaeaea;
@@ -51,16 +56,19 @@ $homeBarBorderColor: #eaeaea;
     top: 0;
     background-color: #fff;
     border-bottom: 1px solid $homeBarBorderColor;
+    font: 400 16px/70px '';
     &-wrapper {
         width: 1200px;
         height: 100%;
         margin: 0 auto;
+        display: flex;
+        justify-content: space-around;
         &-menu {
             display: flex;
             height: 70px;
+            width: 400px;
             align-items: center;
             &-item {
-                font: 400 16px/70px '';
                 margin: 0 20px;
                 transition: color .2s ease;
                 white-space: nowrap;
@@ -69,6 +77,23 @@ $homeBarBorderColor: #eaeaea;
             .select-item {
                 color: $fontColor;
                 font-weight: 500;
+            }
+        }
+        .login {
+            &-user {
+                // font: 400 16px/70px '';
+                margin-right: 30px;
+            }
+            &-button {
+                margin: 20px 0;
+                // width: 90px;
+                // display: inline-block;
+                // height: 30px;
+                // line-height: 30px;
+                // border: 1px solid $fontColor;
+                // color: $fontColor;
+                // text-align: center;
+                // border-radius: 5px;
             }
         }
     }
