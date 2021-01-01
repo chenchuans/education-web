d031efbcb402db94c1dc62baabf0e815019e34c9
chenchuan <1350581922@qq.com>
2021-01-01 20:10:24 +0800

    请求地址改为https

--- src/api/baseUrl.ts
+++ src/api/baseUrl.ts
@@ -1,4 +1,4 @@
-const baseUrl: string = 'http://shuquedu.com:8088/eduweb/v1';
+const baseUrl: string = 'https://shuquedu.com:8088/eduweb/v1';
 // const imageUrl: string = 'http://34.96.172.236:8088/eduweb/v1/resource/';
 const imageUrl: string = 'https://shuquedu.com:8088/eduweb/v1/resource/';
 
